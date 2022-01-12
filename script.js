720a7b503680dff52c6d7a7c330787a5ea87be31
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-01-12 12:01:29 -0600

    Restructuring and continuing to build

--- script.js
+++ script.js
@@ -1,26 +1,96 @@
+'use strict'
+
+const header = document.querySelector('header')
+const main = document.querySelector('main')
 const gameContainer = document.getElementById("game");
+const btnStart = document.querySelector('.btn__start')
+const difficultyMenu = document.querySelector('.difficulty')
+const btnEasy = document.querySelector('.difficulty__easy')
+const btnNorm = document.querySelector('.difficulty__norm')
+const btnHard = document.querySelector('.difficulty__hard')
 const h1 = document.querySelector('h1');
 const game = document.querySelector('#game')
+const best = localStorage.memoryMatchGameBestScore;
+
 let scoreboard;
 let matches = 0;
 let card1;
 let card2;
+let difficulty;
 let score = 0;
-let best = parseInt(localStorage.memoryMatchGameBestScore);
 let checking = false;
+let tempDeck;
+let gameDeck;
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+const BANDPICS = [
+  "cardpic1",
+  "cardpic2",
+  "cardpic3",
+  "cardpic4",
+  "cardpic5",
+  "cardpic6",
+  "cardpic7",
+  "cardpic8",
+  "cardpic9",
+  "cardpic10",
+  "cardpic11",
+  "cardpic12",
+  "cardpic13",
+  "cardpic14",
+  "cardpic15",
+  "cardpic16",
+  "cardpic17",
+  "cardpic18",
+  "cardpic19",
+  "cardpic20",
+  "cardpic21",
+  "cardpic22",
+  "cardpic23",
+  "cardpic24",
+  "cardpic25",
+  "cardpic26",
+  "cardpic27",
+  "cardpic28",
+  "cardpic29",
+  "cardpic30",
 ];
+
+
+// Show difficulty menu
+btnStart.addEventListener('click',showDifficultyMenu)
+
+function showDifficultyMenu() {
+  btnStart.classList.add('hidden')
+  difficultyMenu.classList.remove('hidden')
+}
+
+
+difficultyMenu.addEventListener('click',startGameHandler.bind(this))
+
+// Function to select difficulty, start game
+function startGameHandler(e){
+  e.preventDefault();
+  if(!e.target.classList.contains('btn__difficulty')) return;
+  if(e.target.classList.contains('btn__difficulty--easy')) {difficulty = 'easy'}
+  if(e.target.classList.contains('btn__difficulty--norm')) {difficulty = 'norm'}
+  if(e.target.classList.contains('btn__difficulty--hard')) {difficulty = 'hard'}
+  setDifficulty(difficulty)
+  setDeckSize(difficulty)
+  createCards(gameDeck)
+  revealGame()
+}
+
+// Function to hide start menu and show the game
+function revealGame() {
+  header.classList.add('playing')
+  setTimeout(function(){header.classList.add('hidden')},1000)
+  main.classList.remove('hidden')
+}
+
+function setDifficulty(dif) {
+  gameContainer.classList.remove('easy','norm','hard')
+  gameContainer.classList.add(dif)
+}
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -45,19 +115,41 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuffledCards = shuffle(BANDPICS);
 
-// this function loops over the array of colors
+difficulty = 'easy';
+setDeckSize(difficulty)
+
+function setDeckSize(dif){
+  if (dif === 'easy'){tempDeck = shuffledCards.slice(-5)}
+  if (dif === 'norm'){tempDeck = shuffledCards.slice(-10)}
+  if (dif === 'hard'){tempDeck = shuffledCards.slice(-20)}
+  gameDeck = [...tempDeck,...tempDeck]
+  shuffle(gameDeck);
+}
+
+
+
+// this function loops over the array of band pics
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
+function createCards(cardsArr) {
+  for (let card of cardsArr) {
+    // create a new card
     const newDiv = document.createElement("div");
+    const newInner = document.createElement("div");
+    const newFront = document.createElement("div");
+    const newBack = document.createElement("div");
+    
+    newDiv.classList.add('card');
+    newDiv.classList.add(card)
+    newInner.classList.add('card-inner')
+    newFront.classList.add('card-side', 'card-side__front')
+    newBack.classList.add('card-side', 'card-side__back')
 
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
-
+    newDiv.append(newInner)
+    newInner.append(newFront)
+    newInner.append(newBack)
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
 
@@ -71,6 +163,7 @@ function createDivsForColors(colorArray) {
   displayScore(scoreDiv)
   gameContainer.append(scoreDiv)
 }
+
 
 // TODO: Implement this function!
 function handleCardClick(e) {
@@ -114,10 +207,9 @@ function handleCardClick(e) {
   displayScore(scoreboard);
 
   // win logic
-  if(matches === COLORS.length/2){
+  if(matches === BANDPICS.length/2){
     h1.textContent = 'YOU WIN!'
     storeHiScore();
-    displayScore(scoreboard);
     const newGameBtn = document.createElement('button')
     newGameBtn.textContent = 'Play Again?'
     newGameBtn.addEventListener('click', function(e){
@@ -133,18 +225,15 @@ function resetGame(){
   matches = 0;
   h1.textContent = 'Memory Game!'
   game.innerHTML = ''
-  createDivsForColors(shuffledColors);
-  displayScore(scoreboard);
+  createCards(gameDeck);
+  displayScore(scoreboard)
 }
 
 function storeHiScore(){
   if(!best || best > score){ localStorage.setItem('memoryMatchGameBestScore', score) } 
 }
 
-function displayScore(div){ 
-  best = parseInt(localStorage.memoryMatchGameBestScore);
-  div.innerHTML = best ? `Score: ${score} <br> Best: ${best}` : `Score: ${score}` 
-}
+function displayScore(div){ div.innerHTML = best ? `Score: ${score} <br> Best: ${best}` : `Score: ${score}` }
 
 // when the DOM loads
-createDivsForColors(shuffledColors);
+// createCards(gameDeck);
